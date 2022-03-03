fec994c9159226c66881e56abe8450cd84a2b582
ginsueddy <ginsu.eddy@gmail.com>
2022-03-02 21:29:31 -0800

    created react project

---
+++ frontend/src/components/screens/Home/index.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+const Button = styled.button``;
+
+const Home = () => {
+    return (
+        <div>
+            <div>
+                <h1>Your perfect</h1>
+                <h1>Friday night</h1>
+                <h1>is waiting</h1>
+            </div>
+            <h2>Tell us what you love,</h2>
+            <h2>be with who you love</h2>
+            <Button />
+        </div>
+    )
+};
+
+export default Home;
