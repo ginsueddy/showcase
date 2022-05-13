cc345f7d75bcd6134cf62fc9207adf2043255249
ginsueddy <ginsu.eddy@gmail.com>
2022-05-12 22:52:51 -0700

    finished itineary

--- frontend/src/components/screens/Logistics/index.jsx
+++ frontend/src/components/screens/Logistics/index.jsx
@@ -1,8 +1,65 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import useWindowDimensions from '../../../hooks/useWindowDimensions';
 import Header from '../../shared/Header';
 import Footer from "../../shared/Footer";
+import LoadingSpinner from '../../shared/LoadingSpinner';
+import Itinerary from '../Itinerary';
+
+const logisticsQuiz = [
+    {
+        question: 'Where are you going?',
+        responseType: 'select',
+        stage: 'eventDetails',
+        options: ['University District', 'Capitol Hill', 'Ballard', 'Fremont', 'International District', 'I don\'t know yet']
+    },
+    {
+        question: 'What time are you going?',
+        responseType: 'select',
+        stage: 'eventDetails',
+        options: ['Morning (8am-12pm)', 'Day (12pm-4pm)', 'Evening (4pm-8pm)', 'Late Night (8pm-12am)', 'I don\'t know yet']
+    },
+    {
+        question: 'What\'s your budget?',
+        responseType: 'select',
+        stage: 'eventDetails',
+        options: ['N/A', '$', '$$', '$$$', '$$$$', 'I don\'t know yet']
+    },
+    {
+        question: 'You\'re almost there!',
+        responseType: 'summary',
+        stage: 'eventDetails'
+    },
+    {
+        question: 'How active are you?',
+        responseType: 'select',
+        stage: 'preferences',
+        options: ['I\'m always outdoors', 'It depends on the day', 'I\'m active with friends', 'I prefer to stay in']
+    },
+    {
+        question: 'What\'s your favorite way to spend free time?',
+        responseType: 'select',
+        stage: 'preferences',
+        options: ['Trying a new restaurant', 'See what\'s new on Netflix', 'Reading a good book', 'Spending time outside', 'Something else']
+    },
+    {
+        question: 'What\'s your sign?',
+        responseType: 'select',
+        stage: 'preferences',
+        options: ['Aries', 'Libra', 'Aquarius', 'Scorpio', 'Taurus', 'Capricorn', 'Gemini', 'Virgo', 'Cancer', 'Saggitarius', 'I don\'t know']
+    },
+    {
+        question: 'What genre of music do you listen to?',
+        responseType: 'select',
+        stage: 'preferences',
+        options: ['Pop // Top 50', 'Country Pop', 'EDM', 'HipHop // Rap', 'Something else']
+    },
+    {
+        question: 'Hang tight! We\'re making your plans',
+        responseType: 'summary',
+        stage: 'itinerary'
+    }
+];
 
 const HeaderText = styled.div`
     font-family: 'Outfit';
@@ -24,7 +81,7 @@ const Text = styled.div`
     letter-spacing: 1px;
 `;
 
-const QuizOption = ({ text }) => {
+const QuizOption = ({ text, onClick }) => {
     const [hovered, setHoverd] = useState(false);
 
     return (
@@ -42,6 +99,7 @@ const QuizOption = ({ text }) => {
             }}
             onMouseEnter={() => setHoverd(!hovered)}
             onMouseLeave={() => setHoverd(!hovered)}
+            onClick={onClick}
         >
             <Text style={{ color: hovered ? 'white' : 'black' }}>{text}</Text>
         </div>
@@ -51,25 +109,61 @@ const QuizOption = ({ text }) => {
 const Logistics = () => {
     const { height, width } = useWindowDimensions();
 
+    const [index, setIndex] = useState(0);
+    const [showItinerary, setShowItinerary] = useState(false);
+
+    useEffect(() => {
+        if (index === logisticsQuiz.length - 1) {
+            setTimeout(() => {
+                setShowItinerary(true);
+            }, 3000)
+        }
+
+    }, [index])
+    
     return (
         <>
             <Header />
-                <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', paddingTop: 24 }}>
-                    <div style={{ display: 'flex', justifyContent: 'space-between', width: width * 0.75, marginBottom: 40 }}>
-                        <Text style={{ fontSize: 20 }}>1. Event Details</Text>
-                        <Text style={{ fontSize: 20 }}>2. Preferences</Text>
-                        <Text style={{ fontSize: 20 }}>3. Your Custom Itinerary</Text>
+            {
+                showItinerary ? (
+                    <Itinerary />
+                ) : (
+                    <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', paddingTop: 24, height: height * 0.7 }}>
+                        <div style={{ display: 'flex', justifyContent: 'space-between', width: width * 0.75, marginBottom: 40 }}>
+                            <Text style={{ fontSize: 20, fontWeight: logisticsQuiz[index].stage === 'eventDetails' ? 'bold' : 'normal' }}>1. Event Details</Text>
+                            <Text style={{ fontSize: 20, fontWeight: logisticsQuiz[index].stage === 'preferences' ? 'bold' : 'normal' }}>2. Preferences</Text>
+                            <Text style={{ fontSize: 20, fontWeight: logisticsQuiz[index].stage === 'itinerary' ? 'bold' : 'normal' }}>3. Your Custom Itinerary</Text>
+                        </div>
+                        <HeaderText>{logisticsQuiz[index].question}</HeaderText>
+                        {
+                            logisticsQuiz[index].options && (
+                                <div style={{ marginTop: 16 }}>
+                                    {
+                                        logisticsQuiz[index].options.map((option) => <QuizOption text={option} onClick={() => setIndex(index + 1)} />)
+                                    }
+                                </div>
+                            )
+                        }
+                        {
+                            (logisticsQuiz[index].responseType === 'summary' && logisticsQuiz[index].stage === 'eventDetails') && (
+                                <div style={{ display: 'flex', flex: 1, flexDirection: 'column', alignItems: 'center', justifyContent: 'space-evenly' }}>
+                                    <Text style={{ fontWeight: 'lighter' }}>Fremont, Saturday, March 12th, 10am - 2pm, $$$ Budget</Text>
+                                    <Text style={{ fontWeight: 'lighter' }}>Now that we've got your logistics down, we'll ask you a couple questions about your preferences and what you like to do for fun!</Text>
+                                    <QuizOption text="Let's do it" onClick={() => setIndex(index + 1)} />
+                                </div>
+                            )
+                        }
+                        {
+                            (logisticsQuiz[index].responseType === 'summary' && logisticsQuiz[index].stage === 'itinerary') && (
+                                <div style={{ display: 'flex', flex: 1, flexDirection: 'column', alignItems: 'center', justifyContent: 'space-evenly' }}>
+                                    <Text style={{ fontWeight: 'lighter' }}>Thanks for answering our questions! Sit back while we put together your custom itinerary.</Text>
+                                    <LoadingSpinner />
+                                </div>
+                            )
+                        }
                     </div>
-                    <HeaderText>Where are you going?</HeaderText>
-                    <div style={{ marginTop: 16 }}>
-                        <QuizOption text="University District" />
-                        <QuizOption text="Capitol Hill" />
-                        <QuizOption text="Ballard" />
-                        <QuizOption text="Fremont" />
-                        <QuizOption text="International District" />
-                        <QuizOption text="I don't know yet" />
-                    </div>
-                </div>
+                )
+            }
             <Footer />
         </>
     );
--- frontend/src/components/shared/List/index.jsx
+++ frontend/src/components/shared/List/index.jsx
@@ -10,9 +10,61 @@ const Text = styled.div`
     letter-spacing: 1px;
 `;
 
+const ButtonText = styled.div`
+    font-family: 'Syne';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 16px;
+    text-align: center;
+    letter-spacing: 1px;
+    color: #3E372D;
+`;
 
-const List = ({ listItems }) => {
+
+const List = ({ listItems, isItinerary }) => {
     const { height, width } = useWindowDimensions();
+    
+    if (isItinerary) {
+        return (
+            <div style={{ display: 'flex' }}>
+                <div style={{ width: 12, alignItems: 'center', display: 'flex', flexDirection: 'column', paddingTop: 12 }}>
+                    {
+                        listItems.map((item, index) => {
+                            return (
+                                <>
+                                    <div style={{ width: 12, height: 12, backgroundColor: '#5DA27D', borderRadius: 6 }} />
+                                    <div style={{ width: 1, height: index === listItems.length - 1 ? 224 : 174, backgroundColor: 'black' }} />
+                                </>
+                            )
+                        })
+                    }
+                </div>
+                <div style={{ display: 'flex', flexDirection: 'column', marginLeft: 24 }}>
+                    {
+                        listItems.map((item, index) => (
+                            <>
+                                <Text>{item.stop}</Text>
+                                <Text style={{ fontWeight: 'bold', marginTop: 8 }}>{item.name}</Text>
+                                <Text>{item.address}</Text>
+                                <div onClick={() => console.log()} style={{ backgroundColor: '#5DA27D', paddingLeft: 16, paddingRight: 16, paddingTop: 4, paddingBottom: 4, borderRadius: 30, display: 'flex', alignItems: 'center', alignSelf: 'baseline', marginTop: 16, marginBottom: 40 }}>
+                                    <ButtonText style={{ color: 'white' }}>VISIT</ButtonText>
+                                </div>
+                            </>
+                        ))
+                    }
+                    <div style={{ display: 'flex' }}>
+                        <div onClick={() => console.log()} style={{ backgroundColor: '#E69C57', paddingLeft: 32, paddingRight: 32, paddingTop: 12, paddingBottom: 12, borderRadius: 30, alignItems: 'center', alignSelf: 'baseline', marginTop: 16 }}>
+                            <ButtonText style={{ color: 'white' }}>SAVE PLANS</ButtonText>
+                        </div>
+                        <div style={{ flexDirection: 'column', display: 'flex', marginLeft: 24 }}>
+                            <Text style={{ fontSize: 18 }}>Don't like this?</Text>
+                            <Text style={{ fontSize: 18 }}><u>Get a new plan</u> or <u>start over.</u></Text>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 
     return (
         <div style={{ display: 'flex' }}>
