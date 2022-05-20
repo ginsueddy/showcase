cdea39bd3d9ffeec51f4124975176864e823cf8c
ginsueddy <ginsu.eddy@gmail.com>
2022-05-19 19:27:21 -0700

    made footer responsive

--- frontend/src/components/shared/Footer/index.jsx
+++ frontend/src/components/shared/Footer/index.jsx
@@ -1,22 +1,20 @@
 import styled from 'styled-components';
 import useWindowDimensions from '../../../hooks/useWindowDimensions';
 
-const HighlightedHeader = styled.h1`
+const HighlightedHeader = styled.div`
     font-family: 'Syne', sans-serif;
     font-style: normal;
     font-weight: bold;
     font-size: 72px;
-    line-height: 10px;
-    letter-spacing: -2px;
+    letter-spacing: -1px;
     color: #FFFFFF;
 `;
 
-const Text = styled.p`
+const Text = styled.div`
     font-family: 'Outfit', sans-serif;
     font-style: normal;
     font-weight: 300;
     font-size: 14px;
-    line-height: 133%;
     letter-spacing: 1px;
     color: #FFFFFF;
 `;
@@ -27,17 +25,21 @@ const Footer = () => {
     return (
         <div style={{ width, height: height * .2, display: 'flex', flexDirection: 'column', justifyContent: 'flex-end' }}>
             <div style={{ width, height: height * .3, background: "linear-gradient(180deg, #FFFFFF, #5DA27D 60%)", position: 'absolute', zIndex: -1 }} />
-            <div style={{ paddingLeft: '5%', paddingRight: '3%', paddingBottom: 4 }}>
+            <div style={{ paddingLeft: '5%', paddingRight: '3%', paddingBottom: 20 }}>
                 <HighlightedHeader style={{ fontSize: 36 }}>cultured // seattle</HighlightedHeader>
                 <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: 48 }}>
-                    <div style={{ display: 'flex', flexDirection: 'row', width: '18%', justifyContent: 'space-between' }}>
-                        <Text>Contact Us</Text>
-                        <Text>FAQ</Text>
-                        <Text>Instagram</Text>
+                    <div style={{ display: 'flex', flexDirection: 'row', flex: 1 }}>
+                        <Text style={{ marginRight: 16 }}>Contact Us</Text>
+                        <Text style={{ marginRight: 16 }}>FAQ</Text>
+                        <Text style={{ marginRight: 16 }}>Instagram</Text>
                     </div>
-                    <Text>Designed in Seattle, WA</Text>
-                    <div style={{ display: 'flex', flexDirection: 'row', width: '15%', justifyContent: 'space-between' }}>
-                        <Text>Privacy - Terms</Text>
+                    {
+                        width > 800 && (
+                            <Text style={{ flex: 1 }}>Designed in Seattle, WA</Text>
+                        )
+                    }
+                    <div style={{ display: 'flex', flexDirection: 'row' }}>
+                        <Text style={{ marginRight: 16, marginLeft: 16 }}>Privacy - Terms</Text>
                         <Text>&copy; 2022</Text>
                     </div>
                 </div>
